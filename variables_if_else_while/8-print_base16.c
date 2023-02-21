2423deb3a03590967b8e8426e73ea2ca42104265
PhantomMik3 <miguelforex77@gmail.com>
2023-02-21 03:41:19 +0000

    base16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - main, return
+ *Return: Always (0) (Successful)
+ */
+
+int main(void)
+
+{
+	int d;
+	char low;
+
+	for (low = 'a'; low <= 'f'; low++)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
