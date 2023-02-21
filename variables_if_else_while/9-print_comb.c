7cbb3079581c351cc071899651e5c58fcad7a072
PhantomMik3 <miguelforex77@gmail.com>
2023-02-21 03:55:54 +0000

    problem 8, 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -3,25 +3,25 @@
 #include <time.h>
 
 /**
- * main - Tu papa oite
+ * main - main, putchar, return
  * return: always (0) (succesful)
  */
 
-int main(void)
+	int main(void)
 
 {
-    int d;
+	int d;
 
-    for (d = '0'; d <= '9'; d++)
-    {
-    putchar(d);
-    if (d != '9')
-    {
-    putchar(',');
-    putchar(' ');
-    }
-    }
-    putchar('\n');
+	for (d = '0'; d <= '9'; d++)
+{
+	putchar(d);
+	if (d != '9')
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+	putchar('\n');
 	return (0);
 
 }
