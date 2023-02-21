5329699169643278b0ce468232e44804a6bb5d76
PhantomMik3 <miguelforex77@gmail.com>
2023-02-21 02:42:54 +0000

    printing alphabet in lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase
+ * Return: Always (0) (Successful)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
