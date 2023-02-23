e6036f6195b1b7f731f7d4b5eecaec0bf0cada08
PhantomMik3 <miguelforex77@gmail.com>
2023-02-23 17:39:32 +0000

    alphabet in minuscule

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_alphabet - starting point
+ *@lk
+ */
+void print_alphabet(void)
+{
+	char lk;
+
+	for (lk = 'a'; lk <= 'z'; lk++)
+
+	_putchar(lk);
+	_putchar('\n');
+}
