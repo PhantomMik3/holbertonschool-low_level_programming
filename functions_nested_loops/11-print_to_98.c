a0bd3c1aa07352f0aa9fc2d00f909b32522c371b
PhantomMik3 <miguelforex77@gmail.com>
2023-02-24 17:13:27 +0000

    directory for assignment makefiles

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -20,12 +20,12 @@ void print_to_98(int n)
 			printf("%d\n", a);
 		}
 	}
-	else if ( n >= 98)
+	else if (n >= 98)
 	{
 		for (b = n; b >= 98; b--)
 		{
-	
-	
+
+
 			if (b != 98)
 				printf("%d, ", b);
 			else if (b == 98)
