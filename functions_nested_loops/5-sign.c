8d1cfe517c8ffdc403347f5fcbe44137f03f25bb
PhantomMik3 <miguelforex77@gmail.com>
2023-02-24 00:08:14 +0000

    function that prints sign of number depending on value of n

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_sign - entry point
+ *@n: variable
+ *Return: (0)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else if (n == 0)
+	{
+	_putchar('0');
+	return (0);
+	}
+	else if (n < 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+	return (0);
+}
