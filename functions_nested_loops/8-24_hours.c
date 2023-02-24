ec36fc532738918be526a389509905107e4bc56d
PhantomMik3 <miguelforex77@gmail.com>
2023-02-24 02:27:41 +0000

    print every minute of the day from start to end

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - starting point
+ * Return: Ending
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; h < 24; h++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
