0230fb911547a7a2301c459fc2a9eabecbaa1c89
PhantomMik3 <miguelforex77@gmail.com>
2023-02-24 03:02:09 +0000

    9 multiplication table

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 /**
  * jack_bauer - starting point
- * Return: Ending
+ * Return: no 0
  */
 
 void jack_bauer(void)
@@ -12,7 +12,7 @@ void jack_bauer(void)
 
 	for (h = 0; h < 24; h++)
 	{
-		for (m = 0; m < 24; m++)
+		for (m = 0; m < 60; m++)
 		{
 		_putchar((h / 10) + '0');
 		_putchar((h % 10) + '0');
