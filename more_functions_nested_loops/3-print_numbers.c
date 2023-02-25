c321bd8d27aefc28c20376f13715cc79a2b6d66c
PhantomMik3 <miguelforex77@gmail.com>
2023-02-25 23:35:08 +0000

    print numbers from 0 to 9 followed by new line

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int tt;
+
+	for (tt = 48; tt <= 57; tt++)
+
+	_putchar(tt);
+	_putchar('\n');
+
+}
