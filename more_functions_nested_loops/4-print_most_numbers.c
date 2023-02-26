f59b517c3aeb5bc119d5cd8d82a9d78cd5d378e1
PhantomMik3 <miguelforex77@gmail.com>
2023-02-26 02:03:32 +0000

    print all except 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - start point
+ * @a: Var
+ * new line
+ */
+
+void print_most_numbers(void)
+
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		if (!(a == 50 || a == 52))
+			_putchar(a);
+	}
+	_putchar('\n');
+}
+
