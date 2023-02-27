ae2c9934fe0aecbfed39a3851044d53046e1151b
PhantomMik3 <miguelforex77@gmail.com>
2023-02-27 20:57:18 +0000

    set value of a pointer to different value

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes pointer to an int and updates value to 98
+ * @n: variable
+ */
+
+void reset_to_98(int *n)
+{
+	int *p = n;
+	*p = 98;
+}
