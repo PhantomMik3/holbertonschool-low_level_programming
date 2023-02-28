758747bcdbfb0ee14a6cccd9e051cce1fd8b72e2
PhantomMik3 <miguelforex77@gmail.com>
2023-02-28 19:28:25 +0000

    print elements of array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - print elements of an array
+ * @n: array elements var
+ * @a: pointer var
+ * Return: Always (0)
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	for(i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	putchar('\n');
+}
