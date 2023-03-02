2b2576cc40464026ee5a4bc181586b7d11628c50
PhantomMik3 <miguelforex77@gmail.com>
2023-03-02 18:48:19 +0000

    change all lowercase letters of a string to uppercase letters

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -10,12 +10,12 @@ char *string_toupper(char *s)
 {
 	int e;
 
-	for(e = 0; s[e] != '\0'; e++)
+	for (e = 0; s[e] != '\0'; e++)
 	{
 		if (s[e] >= 'a' && s[e] <= 'z')
 		{
 			s[e] -= 32;
 		}
 	}
-	return(s);
+	return (s);
 }
