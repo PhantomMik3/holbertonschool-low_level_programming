7953b3f67e0657a906973b33563d838f08daed33
PhantomMik3 <miguelforex77@gmail.com>
2023-03-04 17:01:13 +0000

    encrypt given message

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encrypting message
+ * @s: string var
+ * Return: Always s
+ */
+
+char *leet(char *s)
+{
+	char letters[] = "aeotl";
+	char numbers[] = "43071";
+	int cd, bc;
+
+	for (cd = 0; s[cd] != '\0'; cd++)
+	{
+		for (bc = 0; bc < 5; bc++)
+		{
+			if (s[cd] == letters[bc] || s[cd] == letters[bc] - 32)
+			{
+				s[cd] = numbers[bc];
+			}
+		}
+	}
+
+	return(s);
+}
