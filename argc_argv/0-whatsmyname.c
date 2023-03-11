f14f63d883d6a9c715f80ba9cf6c63cffc584445
PhantomMik3 <miguelforex77@gmail.com>
2023-03-11 00:18:07 +0000

    fixed syntax for betty approval

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,12 +3,11 @@
 /**
  * main - Entry to program that prints its name
  * @argc: counter variable
- * @*argv: vector/array of char variable
- * return: in this case (0)
+ * @argv: vector/array of char variable
+ * Return: in this case (0)
  */
 
-int main (int argc __attribute__((unused)), char *argv[])
-
+int main(int argc __attribute__((unused)), char *argv[])
 {
 
 	printf("%s\n", argv[0]);
