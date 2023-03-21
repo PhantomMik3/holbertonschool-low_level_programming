5d153e1682a9be41003fa49767f58ad69345b31c
PhantomMik3 <miguelforex77@gmail.com>
2023-03-21 23:54:20 +0000

    free perro

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -21,4 +21,7 @@ void print_dog(struct dog *d);
 	 * dog_t - struct new name
 	 */
 	typedef struct dog dog_t;
+
+	dog_t *new_dog(char *name, float age, char *owner);
+	void free_dog(dog_t *d);
 #endif
