8eccb84792470decf89b0bf75fa0d754dc9d6651
PhantomMik3 <miguelforex77@gmail.com>
2023-03-23 21:00:56 +0000

    calculator

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -47,6 +47,7 @@ int op_div(int a, int b)
 {
 	if (b == 0)
 	{
+		printf("Error\n");
 		exit(100);
 	}
 	return (a / b);
@@ -63,6 +64,7 @@ int op_mod(int a, int b)
 {
 	if (b == 0)
 	{
+		printf("Error\n");
 		exit(100);
 	}
 	return (a % b);
