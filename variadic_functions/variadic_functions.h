adaf650aca29bbfce45476c64dd953df7641d6fc
PhantomMik3 <miguelforex77@gmail.com>
2023-03-25 22:33:16 +0000

    addind fixes for header file

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,10 @@
+#ifndef variadic_functions_h
+
+#define variadic_functions_h
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
-
-#ifndef variadic_functions_h
-#define variadic_functions_h
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
