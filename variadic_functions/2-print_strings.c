48eb1ebacca316ef81508130a606b64c9dd78fa0
PhantomMik3 <miguelforex77@gmail.com>
2023-03-26 22:20:45 +0000

    adding task that prints strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print strings
+ * @separator: pointer to strings
+ * @n: number of strings passed
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	char *s;
+
+	va_list rags;
+
+	va_start(rags, n);
+
+	for (; i < n; i++)
+	{
+		s = va_arg(rags, char *);
+
+		if (rags == NULL)
+			printf("(nil)");
+		else
+			printf("%s", s);
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+
+	printf("\n");
+	va_end(rags);
+}
