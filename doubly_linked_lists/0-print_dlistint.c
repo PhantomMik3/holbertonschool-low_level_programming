93ebb157c01accb261a5b4e6d6459d016e59494a
PhantomMik3 <miguelforex77@gmail.com>
2023-04-06 15:46:24 +0000

    data specifier was incorrect, now fixed

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -12,13 +12,13 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (h != NULL)
 	{
-		printf("%u\n", h->n);
+		printf("%d\n", h->n);
 		number++;
 		h = h->next;
 	}
 	while (h != NULL)
 	{
-		printf("%u\n", h->n);
+		printf("%d\n", h->n);
 		number++;
 		h = h->prev;
 	}
